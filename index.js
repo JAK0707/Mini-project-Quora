b0adbe8d121fd6d3aacdf86834e76584c9740b02
JAK0707 <jkapoor_be23@thapar.edu>
2024-11-10 16:32:28 +0530

    created array of posts

--- index.js
+++ index.js
@@ -4,15 +4,32 @@ const port = 8080;
 
 app.use(express.urlencoded({extended : true})); //important line , jo bhi url encoded data hoga usko bhi express samajh paayega\
 
-app.get("/",(req,res)=>{
-    res.send("server working well");
-});
 
 const path = require("path");
 app.set("view enjine","ejs");
 
 app.set("views",path.join(__dirname,"views"));
 app.set(express.static(path.join(__dirname,"public")));
+
+let posts = [
+    {
+        username: "jai",
+        content: "hi my name is yo",
+    },
+    {
+        username: "suresh",
+        content: "skdfhkalsjd",
+    },
+    {
+        username: "baldev",
+        content: "hi my name is dfsdlfashld",
+    },
+];
+
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs" , {posts});
+});
 
 app.listen(port,()=>{
     console.log(`listening to port ${port}`);
